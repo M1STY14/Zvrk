f4e19ce1a094c4acbf4e33fc29ba879a6d17b4b3
Leo Kocijan <91187120+M1STY14@users.noreply.github.com>
2026-04-12 14:54:08 +0200

    Added a Game seeder
    Added TODO's for when we create more game engines

---
+++ app/Enums/GameType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+/**
+ * @property-read string $value
+ * @property-read string $name
+ */
+
+enum GameType: string
+{
+    case TicTacToe = 'tic-tac-toe';
+    case Ludo = 'ludo';
+    case Checkers = 'checkers';
+}
