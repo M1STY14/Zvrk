1e90341f8950898e27d1508c92e31292036ccde5
BarbaraRd <barbara.rados22@gmail.com>
2026-04-13 09:09:40 +0200

    Created MoveMade broadcast event

---
+++ app/Events/MoveMade.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MoveMade implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(
+        public string $sessionId,
+        public string $playerId,
+        public int $row,
+        public int $column,
+        public array $board,
+        public ?string $nextPlayerId = null,
+        public ?string $winner = null,
+        public bool $draw = false,
+    ) {
+    }
+
+    public function broadcastOn(): array
+    {
+        return [
+            new PresenceChannel("game.{$this->sessionId}"),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'move.made';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'sessionId' => $this->sessionId,
+            'playerId' => $this->playerId,
+            'row' => $this->row,
+            'column' => $this->column,
+            'board' => $this->board,
+            'nextPlayerId' => $this->nextPlayerId,
+            'winner' => $this->winner,
+            'draw' => $this->draw,
+        ];
+    }
+}
