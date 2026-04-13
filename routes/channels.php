0c2e31699a5926d9beba37bb03ecfc830bfe2e43
BarbaraRd <barbara.rados22@gmail.com>
2026-04-13 10:02:50 +0200

    Defined presence channel for game session

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use App\Models\User;
+use App\Models\GameSession;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('game.{sessionId}', function (User $user, GameSession $session) : array|bool {
+    $isHost = $session->host_user_id === $user->id;
+    $isPlayer = $session->players()->where('user_id', $user->id)->exists();
+    if (!$isHost && !$isPlayer) return false;
+    return [
+        'id' => $user->id,
+        'name' => $user->name,
+    ];
 });
